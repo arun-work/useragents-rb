3bea40f4667af57e3cc2d60bc8059b091ad848b4
Arun Babu <arun.work@rediffmail.com>
2020-01-04 00:24:08 +0530

    match based select updated to be case insensitive

--- lib/useragents.rb
+++ lib/useragents.rb
@@ -16,12 +16,13 @@ module UserAgents
     end
 
     def match_list(send_match_word)
+      send_match_word = send_match_word.downcase
       @@match_list = [] if !@@match_list
-      @@match_list[send_match_word] ||= list.select{|l| l.include?(send_match_word) }
+      @@match_list[send_match_word] ||= list.select{|l| l.downcase.include?(send_match_word) }
     end
 
     def rand_with_match(send_match_word)
-      match_list.sample
+      match_list(send_match_word).sample
     end
 
   end
