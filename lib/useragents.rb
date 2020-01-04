b600093668bac1257b53126255df3aef803a7cd2
Arun Babu <arun.work@rediffmail.com>
2020-01-04 20:45:38 +0530

    fixed implicit conversion error by using hash instead of array

--- lib/useragents.rb
+++ lib/useragents.rb
@@ -16,8 +16,8 @@ module UserAgents
     end
 
     def match_list(send_match_word)
-      send_match_word = send_match_word.to_s.downcase
-      @@match_list ||= [] 
+      send_match_word = send_match_word.downcase
+      @@match_list ||= {}
       @@match_list[send_match_word] ||= list.select{|l| l.downcase.include?(send_match_word) }
     end
 
